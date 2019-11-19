8db246345fff0dc148b46e8b10ee0d45883230fc
ainuo5213 <1660998482@qq.com>
2019-11-19 11:57:00 +0800

    feature: 路由演示

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,22 @@ router.get('/', function (ctx, next) {
 router.get('/bar', function (ctx, next) {
     ctx.body = 'this is a users/bar response';
 });
+router.get('/profile/:username', function (ctx, next) {
+    ctx.body = {
+        username: ctx.params.username,
+    };
+});
+router.get('/loadMore/:username/:pageIndex', function (ctx, next) {
+    ctx.body = {
+        username: ctx.params.username,
+        pageIndex: ctx.params.pageIndex
+    };
+});
+router.post('/login', function (ctx, next) {
+    var _a = ctx.request.body, username = _a.username, password = _a.password;
+    ctx.body = {
+        username: username,
+        password: password
+    };
+});
 module.exports = router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -12,5 +12,22 @@ router.get('/', function (ctx: Context, next: Function) {
 router.get('/bar', function (ctx: Context, next: Function) {
   ctx.body = 'this is a users/bar response'
 });
-
+router.get('/profile/:username', function (ctx: Context, next: Function) {
+  ctx.body = {
+    username: ctx.params.username,
+  }
+});
+router.get('/loadMore/:username/:pageIndex', function (ctx: Context, next: Function) {
+  ctx.body = {
+    username: ctx.params.username,
+    pageIndex: ctx.params.pageIndex
+  }
+});
+router.post('/login', function (ctx: Context, next: Function) {
+  const {username, password} = ctx.request.body;
+  ctx.body = {
+    username: username,
+    password: password
+  }
+});
 module.exports = router;
